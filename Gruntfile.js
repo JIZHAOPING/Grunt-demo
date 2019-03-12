0cbbaeb1ef9221fa2acec7b02fe79fc1d70e2816
JIZHAOPING <670237342@qq.com>
2019-03-12 15:45:34 +0800

    sprite

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,15 @@
 module.exports = function(grunt){
   grunt.initConfig({
-    concat:{
-      options: {
-              separator: ';',
-      
-      },
-    dist: {
-        src: ['rectangle.js', 'calc.js'],
-        dest: 'dist/bundle.js',
-      
-  }
+    sprite:{
+      all: {
+          src: './images/*.png',
+          dest: './images/all.png',
+          destCss: './sprites.css'
+                                                                  
+      }
     }
       
 });
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('default',['concat']);
+  grunt.loadNpmTasks('grunt-spritesmith');
+  grunt.registerTask('default',['sprite']);
 }
